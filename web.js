13bdb603a84039da96fac3d78f289c3e95556ebb
10fu3 <31952653+10fu3@users.noreply.github.com>
2021-03-16 16:21:34 +0900

    js setup

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
-var app = express.createServer();
-var port = process.env.PORT || 3000;
+var app = express();
+var port = process.env.PORT || 8080;
 
 class User{
-  constructor(uid) {
+  constructor() {
 
   }
 }
 
 app.get('/', function (req, res) {
-
+  var user = { name:'太郎', age:32, tel:'080-1234-5678' };
+  res.json(user);
 });
 
 app.listen(port, function(){
