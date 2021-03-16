a600b4d9f28bc0e9bc560c4211c21b72cbaae87b
10fu3 <31952653+10fu3@users.noreply.github.com>
2021-03-16 23:32:40 +0900

    add ejs

--- web.js
+++ web.js
@@ -1,20 +1,25 @@
-var express = require('express');
-let ejs = require('ejs');
-var app = express();
-var port = process.env.PORT || 8080;
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.set('views', __dirname + '/public');
 
-class User{
-  constructor() {
-
-  }
-}
-
 app.get('/', function (req, res) {
-  var user = { name:'太郎', age:32, tel:'080-1234-5678' };
-  res.render("./index.ejs")
+  res.render("./index.ejs",{message:'mes'})
+});
+
+app.get('/user/:name', (req, res) => {
+  let queryUser = req.params.name;
+  res.status(200).send(queryUser);
+});
+
+app.get('/place/:name', (req, res) => {
+  let queryUser = req.params.name;
+  res.status(200).send(queryUser);
+});
+app.get('/profile', (req, res) => {
+  res.status(200).send("プロフィールが表示されます");
 });
 
 app.listen(port, function(){
