9503382b4fc2dd29ce7cf3fcb534f6321c230470
10fu3 <31952653+10fu3@users.noreply.github.com>
2021-03-18 03:45:02 +0900

    Add files via upload

--- src/main/java/net/den3/hackathonWeb/Main.java
+++ src/main/java/net/den3/hackathonWeb/Main.java
@@ -1,6 +1,7 @@
 package net.den3.hackathonWeb;
 
 import io.javalin.Javalin;
+import net.den3.hackathonWeb.Entity.IUser;
 import net.den3.hackathonWeb.Store.*;
 import net.den3.hackathonWeb.View.LoginPage;
 import net.den3.hackathonWeb.View.RegisterPage;
@@ -16,6 +17,9 @@ import java.net.URI;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     private static int getHerokuAssignedPort() {
@@ -40,6 +44,11 @@ public class Main {
         new RegisterPage(app);
         new SelfProfile(app);
 
+        app.get("/debug",ctx->{
+            Map<String, List<IUser>> json = new HashMap<>();
+            json.put("users",userStore.getUsers());
+            ctx.json(json);
+        });
 
         app.after(ctx->{ctx.res.setCharacterEncoding("UTF-8");});
     }
