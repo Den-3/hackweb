d5c4664a9ed70955c425058425092c98bd367742
10fu3 <31952653+10fu3@users.noreply.github.com>
2021-03-18 18:27:39 +0900

    Add files via upload

--- src/main/java/net/den3/hackathonWeb/View/PlaceUser.java
+++ src/main/java/net/den3/hackathonWeb/View/PlaceUser.java
@@ -4,18 +4,35 @@ import io.javalin.Javalin;
 import net.den3.hackathonWeb.Entity.Post;
 import net.den3.hackathonWeb.Main;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PlaceUser {
+    class PlaceData{
+        PlaceData(String nick,String uuid){
+            this.nick = nick;
+            this.uuid = uuid;
+        }
+        String nick;
+        String uuid;
+        String getNick(){
+            return nick;
+        }
+        String getUUID(){
+            return uuid;
+        }
+    }
     public PlaceUser(Javalin javalin){
         javalin.get("/place/:place",ctx->{
             String place = ctx.pathParam("place");
             Map<String,Object> data = new HashMap<>();
-            List<Post> posts = Main.logStore.getPosts().stream().filter(l->l.getFacility().equalsIgnoreCase(place)).collect(Collectors.toList());
-            data.put("users",posts);
+            List<PlaceData> userdata = new ArrayList<>();
+            Main.logStore.getPosts().stream().filter(l->l.getFacility().equalsIgnoreCase(place)).forEach(p->{
+                Main.userStore.getUser(p.getUserID()).ifPresent(u->{
+                    userdata.add(new PlaceData(u.getNick(), u.getUUID()));
+                });
+            });
+            data.put("users",userdata);
             data.put("searchWord",place);
             ctx.render("/WEB-INF/templates/place.html",data);
         });
